6b8d0c5fef466d226af2def282718c8de2cb55b3
daasc <paulojhol@gmail.com>
2022-05-03 19:53:39 -0300

    add firestore

---
+++ functions/services/MarketPlace.js
@@ -0,0 +1,27 @@
+class MarketPlace {
+  constructor({ fireStore }) {
+    this.fireStore = fireStore;
+  }
+
+  async get() {
+
+  }
+
+  async getId(id) {
+
+  }
+  
+  async store() {
+
+  }
+
+  async update() {
+
+  }
+
+  async delete() {
+
+  }
+
+
+}
