f0c303198edb82a895233841bc252f7dd6a25c70
daasc <paulojhol@gmail.com>
2022-05-24 19:40:08 -0300

    add validation of the category

--- functions/validation/category.js
+++ functions/validation/category.js
@@ -3,16 +3,9 @@ const { body } = require('express-validator');
 const validationRules = () => {
   return [
     body("name").notEmpty().withMessage('Campo name obrigatório!'),
-    body("price")
-      .notEmpty().withMessage('Campo name obrigatório!')
-      .isFloat().withMessage('Campo name seu valor deve ser numérico!'),
-    body("categories").notEmpty().withMessage('Campo categories obrigatório!'),
     body("urlImage").notEmpty().withMessage('Campo urlImage obrigatório!'),
-    body("sku").notEmpty().withMessage('Campo sku obrigatório!'),
     body("description").notEmpty().withMessage('Campo description obrigatório!'),
   ]
 }
-
-
 
 module.exports = { validationRules }
