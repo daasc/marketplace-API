3aea9d5d73d824449e3008bbe54ca012456e1753
daasc <paulojhol@gmail.com>
2022-05-25 18:29:46 -0300

    add new request: put, delete, getId

--- functions/routes/category.js
+++ functions/routes/category.js
@@ -1,7 +1,7 @@
 const { Router } = require('express');
 const ServiceCategory = require('../services/category.js');
 const serviceCategory = new ServiceCategory();
-const { validate }  = require('../middleware/validation.js');
+const { validate } = require('../middleware/validation.js');
 const { validationRules } = require('../validation/category');
 
 const router = Router();
@@ -20,7 +20,38 @@ router.post('/', validationRules(), validate, async (req, res) => {
   try {
     const { body: category } = req;
     await serviceCategory.store(category);
-    res.status(200).json({message: 'created with success!'});
+    res.status(200).json({ message: 'category created with success!' });
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
+
+router.get('/:id', async (req, res) => {
+  try {
+    const { params: { id } } = req;
+    const category = await serviceCategory.getId(id);
+    res.status(200).json(category);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+
+});
+router.put('/:id', async (req, res) => {
+  try {
+    const { params: { id }, body: newProduct  } = req;
+    console.log(id, newProduct);
+    await serviceCategory.update({ id, newProduct });
+    res.status(200).json({ message: 'category updated with success!' });
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
+
+router.delete('/:id', async (req, res) => {
+  try {
+    const { params: { id } } = req;
+    await serviceCategory.delete(id);
+    res.status(200).json({ message: 'category deleted with success!' });
   } catch (error) {
     res.status(400).send(error);
   }
--- functions/services/category.js
+++ functions/services/category.js
@@ -11,7 +11,7 @@ class ServiceCategory {
       const categories = [];
       const response = await this._fireStore.get();
       response.forEach(doc => {
-        categories.push(doc.data())
+        categories.push({ ...doc.data(), id: doc.id })
       });
 
       return categories;
@@ -21,10 +21,39 @@ class ServiceCategory {
     }
   }
 
+  async getId(id) {
+    try {
+      const category = await this._fireStore.doc(id).get();
+      if (!category.data()) {
+        throw 'category not found!';
+      }
+      return category.data();
+    } catch (error) {
+      throw error;
+    }
+  }
+
   async store(category) {
     try {
       await this._fireStore.add(category);
     } catch (error) {
+      throw error;
+    }
+  }
+
+  async update({id, newProduct}) {
+    try {
+      await this._fireStore.doc(id).update(newProduct);
+    } catch (error) {
+      throw error;
+    }
+  }
+  async delete(id) {
+    try {
+      await this.getId(id)
+      await this._fireStore.doc(id).delete();
+    } catch (error) {
+      console.log(error);
       throw error;
     }
   }
