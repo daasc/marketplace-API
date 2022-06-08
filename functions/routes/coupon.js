228826e573166b12961da7b4f1b3cf15ba27c8e0
daasc <paulojhol@gmail.com>
2022-06-08 19:25:16 -0300

    add coupon's router

---
+++ functions/routes/coupon.js
@@ -0,0 +1,53 @@
+const { Router } = require('express');
+const ServiceCoupon = require('../services/coupon');
+const serviceCoupon = new ServiceCoupon();
+const router = Router();
+
+router.get('/', async (req, res) => {
+  try {
+    const coupons = await serviceCoupon.get();
+    res.status(200).json(coupons);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
+
+router.get('/:id', async (req, res) => {
+  try {
+    const { params: { id } } = req;
+    const coupon = serviceCoupon.getId(id);
+    res.status(200).json(coupon);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
+
+router.post('/', async (req, res) => {
+  try {
+    const { body: coupon } = req;
+    await serviceCoupon.store(coupon);
+    res.status(200).send('created with success!');
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
+
+router.put('/:id', async (req, res) => {
+  try {
+    const { params: id, body: coupon } = req;
+    await serviceCoupon.update(id, coupon);
+    res.status(200).send('coupon updated with success!');
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
+
+router.delete('/:id', async (req, res) => {
+  try {
+    const { params: id } = req;
+    await serviceCoupon.remove(id);
+    res.status(200).send('coupon deleted with success!');
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
