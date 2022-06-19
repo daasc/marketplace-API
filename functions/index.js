8e070fbdc4837af77a579a841e588eae87680b61
daasc <paulojhol@gmail.com>
2022-06-19 14:35:07 -0300

    add cors

--- functions/index.js
+++ functions/index.js
@@ -2,11 +2,12 @@ require('dotenv/config');
 const functions = require("firebase-functions");
 const bodyParser = require('body-parser')
 const express = require('express');
+const cors = require('cors');
 const routes = require('./routes');
 
 const app = express();
 
-
+app.use(cors({ origin: '*' }))
 app.use(express.json());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
